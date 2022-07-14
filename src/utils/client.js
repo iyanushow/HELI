101b80370bd3956a164fb81ff591174c102ebb8e
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-14 11:12:57 +0100

    finally fixed bloody cors

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,6 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+
+const httpLink = new HttpLink({ uri: "https://heli-server.herokuapp.com/gql" });
 
 export const client = new ApolloClient({
-  uri: "https://heli-gql.herokuapp.com/",
+  link: httpLink,
   cache: new InMemoryCache(),
 });
